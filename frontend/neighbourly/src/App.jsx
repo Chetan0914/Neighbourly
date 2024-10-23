f2648af902d579b682ea83eb2bdc5c554c9d69d1
Ekansh-31 <ekanshsrivastava31@gmail.com>
2024-10-23 16:06:57 +0530

    Neighbourly Website

---
+++ frontend/neighbourly/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+<div class="bg-blue-100 min-h-screen flex items-center justify-center">
+  <div class="text-xl font-medium m-20 p-0 border-2 border-black shadow-2xl rounded-xl h-32 w-32 bg-red-300 flex items-center justify-center">
+    Hello World
+  </div>
+</div>
+  )
+}
+
+export default App
